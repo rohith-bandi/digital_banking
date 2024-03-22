675a481b6914a20907cc02a9ead55430392aaf36
BANDI ROHITH REDDY <139671817+Rohith-github@users.noreply.github.com>
2024-03-22 20:11:30 +0530

    Modify the changes

--- admin.py
+++ admin.py
@@ -1,144 +1,157 @@
 import json
 import datetime
-
-def deposit(acc_no, amount):
+def deposit(acc_no):
     try:
-        with open("usersdetails.json", "r+") as js:
-            ud = json.load(js)
+        with open ("usersdetails.json","r+") as js:
+            ud=json.load(js)
             for i in ud.values():
-                if i["Admin/User"] == "User" and i["Account_number"] == acc_no:
-                    i["Balance"] += amount
-                    js.seek(0)
-                    json.dump(ud, js)
-                    date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
-                    add_transaction(acc_no, "Admin", amount, date)
-                    print("Amount has been deposited")
+                if (i["Admin/User"]=="User" and i["Account_number"]==None):
+                    print("He/she dont have a bank account please create a bank account first")
                     break
-    except Exception as e:
-        print(f"Error: {str(e)}")
+                if  (i['Admin/User'] == 'User'and i["Account_number"]==acc_no):
+                    amount=int(input("Enter amount to be deposited: "))
+                    i["Balance"]+=amount
+                    js.seek(0)
+                    json.dump(ud,js)
+                    print("Amount has been deposited")
+                    with open ("transaction_details.json","r+") as js1:
+                        ud1=json.load(js1)
+                        id=len(ud1)+1
+                        date = datetime.datetime.now().strftime( "%d/%m/%Y %H:%M" )
+                        ud2={id:{"From_acc":acc_no,"To_acc":"Admin","Amount":amount,"Date":date}}
+                        ud1.update(ud2)
+                        js1.seek(0)
+                        json.dump(ud1,js1)
+                        break
+    except:
+        print("File doesn't exist")
 
-def withdrawal(acc_no, amount):
+def withdrawal(acc_no):
     try:
-        with open("usersdetails.json", "r+") as js:
-            ud = json.load(js)
+        with open ("usersdetails.json","r+") as js:
+            ud=json.load(js)
             for i in ud.values():
-                if i["Admin/User"] == "User" and i["Account_number"] == acc_no:
-                    if amount > i["Balance"]:
+
+                if (i["Admin/User"]=="User" and i["Account_number"]==None):
+                    print("He/she dont have a bank account please create a bank account first")
+                    break
+                if  (i['Admin/User'] == 'User'and i["Account_number"]==acc_no):
+                    amount=int(input("Enter amount to be withdrawed: "))
+                    if(amount>i["Balance"]):
                         print("Not enough Balance")
                     else:
-                        i["Balance"] -= amount
+                        print("Amount has been withdrawed")
+                        i["Balance"]-=amount
                         js.seek(0)
-                        json.dump(ud, js)
-                        date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
-                        add_transaction("Admin", acc_no, amount, date)
-                        print("Amount has been withdrawn")
-                    break
-    except Exception as e:
-        print(f"Error: {str(e)}")
-
-def add_transaction(from_acc, to_acc, amount, date):
-    try:
-        with open("transaction_details.json", "r+") as js1:
-            ud1 = json.load(js1)
-            id = len(ud1) + 1
-            ud2 = {id: {"From_acc": from_acc, "To_acc": to_acc, "Amount": amount, "Date": date}}
-            ud1.update(ud2)
-            js1.seek(0)
-            json.dump(ud1, js1)
-    except Exception as e:
-        print(f"Error: {str(e)}")
-
+                        json.dump(ud,js)
+                        with open ("transaction_details.json","r+") as js1:
+                            ud1=json.load(js1)
+                            id=len(ud1)+1
+                            date = datetime.datetime.now().strftime( "%d/%m/%Y %H:%M" )
+                            ud2={id:{"From_acc":"Admin","To_acc":acc_no,"Amount":amount,"Date":date}}
+                            ud1.update(ud2)
+                            js1.seek(0)
+                            json.dump(ud1,js1)
+                            break
+    except:
+        print("File doesn't exist")
+        
 def get(acc_no):
     try:
-        with open("usersdetails.json", "r") as js:
+        with open ("usersdetails.json","r") as js:
             ud = json.load(js)
             for i in ud.values():
-                if i["Admin/User"] == "User" and i["Account_number"] == acc_no:
+                if(i["Admin/User"]=="User" and i["Account_number"]==acc_no):
                     return i["Username"]
             return "Bank"
-    except Exception as e:
-        print(f"Error: {str(e)}")
+    except:
+        print("File doesn't exist")
+        
+            
 
 def getacc(username):
     try:
-        with open("usersdetails.json", "r") as js:
+        with open ("usersdetails.json","r") as js:
             ud = json.load(js)
+        
             for i in ud.values():
-                if i["Admin/User"] == "User" and i["Username"] == username:
-                    if i["Account_number"] is None:
-                        print("You don't have an account, please create an account")
+            
+                if (i["Admin/User"]=="User" and i["Username"]==username):
+                    if(i["Account_number"]==None):
+                        print("You dont have an account please create an account")
                         return 0
                     else:
                         return i["Account_number"]
-    except Exception as e:
-        print(f"Error: {str(e)}")
-
+    except:
+         print("Fiel doesn't exist")
+         
 def viewtrans():
     try:
-        with open("transaction_details.json", "r") as js1:
-            ud1 = json.load(js1)
-            if not ud1:
+        with open("transaction_details.json","r") as js1:
+            ud=json.load(js1)
+            if ud=={}:
                 print("No transactions yet")
-            else:
-                for i in ud1.values():
-                    print("From: ", i["From_acc"], " - ", get(i["From_acc"]),
-                          "  To: ", i["To_acc"], " - ", get(i["To_acc"]), " Amount: ", int(i["Amount"]), " Date: ", i["Date"])
-    except Exception as e:
-        print(f"Error: {str(e)}")
+            
+            for i in ud.values():
+                print("From: ",i["From_acc"]," - ",get(i["From_acc"]),"  To: ",i["To_acc"]," - ",get(i["To_acc"])," Amount: ",int(i["Amount"])," Date: ",i["Date"])
+    except:
+        print("File doesn't exist")
+    
 
 def bankbalance():
     try:
-        with open("usersdetails.json", "r") as f:
-            ud = json.load(f)
-            total_balance = sum(i["Balance"] for i in ud.values() if i["Admin/User"] == "User" and i["Account_number"] is not None)
-            return total_balance
-    except Exception as e:
-        print(f"Error: {str(e)}")
+        with open("usersdetails.json","r") as f:
+            ud=json.load(f)
+            s=0
+            for i in ud.values():
+                if i["Admin/User"]=="User":
+                    if i["Account_number"]!=None:
+                         s=s+i["Balance"]
+            return s
+    except:
+        print("File doesn't exist")
 
-def main():
-    while True:
-        print("\nEnter an option")
-        print("1. Deposit")
-        print("2. Withdrawal")
-        print("3. View transactions")
-        print("4. View bank balance")
-        print("5. View the feedback of users")
-        print("6. Exit")
-        opt = int(input("Enter an option: "))
 
-        if opt == 1:
-            username = input("Enter the account name: ")
-            amount = int(input("Enter amount to be deposited: "))
-            k = getacc(username)
-            if k != 0:
-                deposit(k, amount)
-
-        elif opt == 2:
-            username = input("Enter the account name: ")
-            amount = int(input("Enter amount to be withdrawn: "))
-            k = getacc(username)
-            if k != 0:
-                withdrawal(k, amount)
-
-        elif opt == 3:
-            viewtrans()
-
-        elif opt == 4:
-            bal = bankbalance()
-            print(f"\nThe total bank balance available is: {bal} rupees")
-
-        elif opt == 5:
-            try:
-                with open("feedback.txt", "r") as f:
-                    print(f.read())
-            except Exception as e:
-                print(f"Error: {str(e)}")
-
-        elif opt == 6:
-            break
-
-        else:
-            print('Invalid option')
-
-if __name__ == "__main__":
-    main()
+def display():
+    
+    print()
+    print("Enter an option")
+    print("1.Deposit")
+    print("2.Withdrawal")
+    print("3.View transactions")
+    print("4.View bank balance")
+    print("5.View the feedback of users")
+    print("6.Exit")
+    print()
+    opt=int(input("Enter an option: "))
+    if (opt==1):
+        username=input("Enter the account name: ")
+        k=getacc(username)
+        if(k!=0):
+            deposit(k)
+        display()
+    elif(opt==2):
+        username=input("Enter the account name: ")
+        k=getacc(username)
+        if(k!=0):
+            withdrawal(k)
+        display()
+    elif(opt==3):
+        viewtrans()
+        display()
+    elif(opt==4):
+        bal=bankbalance()
+        print("\nThe total bank balance available is:",bal,"rupees")
+        display()
+    elif(opt==5):
+        try:
+            with open("feedback.txt","r") as f:
+                print(f.read())
+        except:
+            print("File doesn't exist")
+        display()
+    elif(opt==6):
+        return 0
+    else:
+        print('Invalid option')
+        display()
--- main.py
+++ main.py
@@ -4,139 +4,138 @@ import account
 import admin
 
 
-def valid_password(p):
-    if len(p) >= 8 and len(p) <= 15 and re.search("[A-Z]", p) and re.search("[a-z]", p) and re.search("[0-9]", p) and re.search("[@#*$]", p):
-        if not re.search("\s", p):
+def validpassword(p):
+    if len(p)>=8 and len(p)<=15 and re.search("[A-Z]",p) and re.search("[a-z]",p) and re.search("[0-9]",p) and re.search("[@#*$]",p):
+        if not re.search("\s",p):
             return True
         else:
             return False
     else:
         return False
 
-
+    
 def register():
     try:
-        with open("usersdetails.json", "r+") as f:
-            jdic = json.load(f)
-            id = len(jdic) + 1
-            while True:
-                username = input("Enter your Username:")
+        with open("usersdetails.json","r+") as f:
+            jdic=json.load(f)
+            id=len(jdic)+1
+            while 1:
+                username=input("Enter your Username:")
                 for i in jdic:
-                    if jdic[i]["Username"] == username:
-                        print("Username already exists")
+                    if jdic[i]["Username"]==username :
+                        print("Username already exist")
                         break
                 else:
-                    break
+                    break    
             instructor()
-            while True:
-                password = input("Enter your Password:")
-                if not valid_password(password):
+            while 1:
+                password=input("Enter your Password:")
+                if validpassword(password)==False:
                     print("Invalid Password")
                 else:
                     break
-            while True:
-                cpassword = input("Confirm password:")
-                if password == cpassword:
-                    while True:
-                        name = input("Enter your name:")
-                        if re.search(r"^[a-zA-Z]+$", name):
+            while 1:    
+                cpassword=input("Confirm password:")
+                if password==cpassword:
+                    while 1:
+                        name=input("Enter your name:")
+                        if re.search(r"^[a-zA-Z]+$",name):
                             break
                         else:
                             print("Name should contain only alphabets")
-                    dic = {id: {"Username": username, "Password": password, "Name": name, "Account_number": None, "Admin/User": "User"}}
+                    dic={id:{"Username":username,"Password":password,"Name":name,"Account_number":None,"Admin/User":"User"}}
                     jdic.update(dic)
                     f.seek(0)
-                    json.dump(jdic, f)
+                    json.dump(jdic,f) 
                     print("Registered successfully")
-                    break
+                    break    
                 else:
                     print("Password not matched")
-    except Exception as e:
-        print(f"Error: {str(e)}")
+    except:
+        print("File doesn't exist")
 
-
+                
 def login():
-    flag = 0
-    flag1 = 0
+    flag=0
+    flag1=0
     try:
-        with open("usersdetails.json", "r") as f:
-            jdic = json.load(f)
-            for _ in range(3):
-                username = input("Enter your Username:")
+        with open("usersdetails.json","r") as f:
+            jdic=json.load(f)
+            for j in range(3):
+                username=input("Enter your Username:")
                 for i in jdic:
-                    if jdic[i]["Username"] == username:
-                        flag = 1
+                    if jdic[i]["Username"]==username:
+                        flag=1
                         break
                 else:
-                    print("Username does not exist")
-                if flag == 1:
+                    print("Username not exist")
+                if(flag==1):
                     break
-                if i == 2:
+                if i==2:
                     return False
-            if flag == 1:
-                for _ in range(3):
-                    password = input("Enter your Password:")
+            if flag==1:
+                for j in range(3):
+                    Password=input("Enter your Password:")
                     for i in jdic:
-                        if jdic[i]["Password"] == password and jdic[i]["Username"] == username:
-                            flag1 = 1
+                        if jdic[i]["Password"]==Password and jdic[i]["Username"]==username:
+                            flag1=1
                             break
                     else:
                         print("Password is incorrect")
-                    if flag1 == 1:
+                    if(flag1==1):
                         break
-                    if i == 2:
+                    if i==2:
                         return False
-                if flag == 1 and flag1 == 1:
-                    print("Logged in successfully")
+                if flag==1 and flag1==1:
+                    print("Logined Successfulluy")
                     return username
-    except Exception as e:
-        print(f"Error: {str(e)}")
+    except:
+         print("File doen't exist")
 
-
+         
 def admin1(username):
     try:
-        with open("usersdetails.json", "r") as js:
+        with open ("usersdetails.json","r") as js:
             ud = json.load(js)
             for i in ud.values():
-                if i["Admin/User"] == "User" and i["Username"] == username:
-                    return "user"
-                if i["Admin/User"] == "Admin" and i["Username"] == username:
-                    return "admin"
-    except Exception as e:
-        print(f"Error: {str(e)}")
-
-
-def main():
-    print("************************************")
-    print("       WELCOME TO THE RSS BANK      ")
-    print("************************************\n")
-
-    while True:
-        print("1. REGISTER OR 2. LOGIN")
-        x = input("Enter your option:")
-        if x == "1":
-            register()
-            continue
-        elif x == "2":
-            k = login()
-            if k:
-                if admin1(k) == "user":
-                    account.display(k)
-                if admin1(k) == "admin":
-                    print("Welcome to the admin page!")
-                    admin.display()
-            else:
-                continue
-            break
-        else:
-            print("Enter correct option")
-
-
-def instructor():
+                if (i["Admin/User"]=="User" and i["Username"]==username):
+                        return "user"
+                if(i["Admin/User"]=="Admin" and i["Username"]==username):
+                        return "admin"
+    except:
+        print("File doesn't exist")
+                
+def  instructor():
     print("\nPassword should contain at least 8 characters and at most 15 characters")
     print("Password should contain at least 1 uppercase and 1 special character")
     print("Password should contain lowercase and digits\n")
 
+    
+print("************************************")
+print("       WELCOME TO THE RSS BANK      ")
+print("************************************\n")
+    
+while 1:
+    print("1.REGISTER OR 2.LOGIN")
+    x=input("Enter your option:")
+    if x=="1":
+        register()
+        print("\n\n\n")
+        continue      
+    elif x=="2":
 
-if __name__ == "__main__":
-    main()
+        k=login()
+
+        if(k!=False):
+            if(admin1(k)=="user"):
+                account.display(k)
+            if(admin1(k)=="admin"):
+                print("Welcome to the admin page!")
+                admin.display()
+            print("\n\n\n")    
+        else:
+            print("\n\n\n")
+            continue
+        break
+    else:
+        print("Enter correct option")    
